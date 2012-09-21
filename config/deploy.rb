87ea0015d672e51106a133bfd72d2808de8f68fd
elja <a.eliseev@itransition.com>
2012-09-21 15:02:35 +0300

    Issue-000 capistrano setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,35 +21,19 @@ set :default_environment, {
     'LANG'   => 'en_US.UTF-8'
 }
 
-namespace :assets do
-  desc "Create a symlink for application.css (used by static pages)"
-  task :static, :roles => :web, :except => { :no_release => true } do
-    %w(application.css).each do |asset|
-      file = capture "cd #{shared_path}/assets && ruby -ryaml -e 'p YAML.load_file(\"manifest.yml\")[\"#{asset}\"]'"
-      run "cd #{shared_path}/assets && ln -sf #{file.chomp} #{asset}"
-    end
+namespace :deploy do
+  task :symlink_config, roles: :app do
+    run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+
+  task :seed, roles: :app do
+    run "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec rake db:seed"
   end
 end
-
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/"
-
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
-
-  task :bundle_new_release, :roles => :app do
-    bundler.create_symlink
-
-    run "cd #{release_path} && bundle install --without development test"
-  end
-end
-
 
 before 'deploy:setup', 'rvm:install_rvm'
 before 'deploy:setup', 'rvm:install_ruby'
+
 after "deploy:finalize_update", "deploy:symlink_config"
 after "deploy:update_code", "deploy:migrate"
 after "deploy:restart", "deploy:cleanup"
